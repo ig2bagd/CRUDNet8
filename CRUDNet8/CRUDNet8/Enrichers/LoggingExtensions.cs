5924fde61fb64b5e14ac58b8a8b0ca9f3418cea6
steviep <ig2bagd@yahoo.com>
2024-06-11 21:41:16 -0700

    Fixed Serilog timestamp discrepancies

---
+++ CRUDNet8/CRUDNet8/Enrichers/LoggingExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Serilog.Configuration;
+using Serilog;
+
+namespace CRUDNet8.Enrichers;
+
+// https://stackoverflow.com/questions/62212569/how-to-get-serilog-to-use-custom-enricher-from-json-config-file
+public static class LoggingExtensions
+{
+    public static LoggerConfiguration WithTimestamp(
+        this LoggerEnrichmentConfiguration enrich)
+    {
+        if (enrich == null)
+            throw new ArgumentNullException(nameof(enrich));
+
+        return enrich.With<TimestampEnricher>();
+    }
+}
